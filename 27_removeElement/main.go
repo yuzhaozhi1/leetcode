718e3f46c707a9451f4de99aaef506d6b9cf2278
yuzhaozhi1 <75839232+yuzhaozhi1@users.noreply.github.com>
2022-11-09 22:57:24 +0800

    add

--- 27_removeElement/main.go
+++ 27_removeElement/main.go
@@ -3,24 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{3, 2, 2, 3, 4, 3, 4, 3}
+	nums := []int{3, 2, 2, 3, 4}
 	element := removeElement(nums, 3)
 	fmt.Println(element)
 }
 
 func removeElement(nums []int, val int) int {
-	i := 0
-	length := len(nums)
-	j := length - 1
-	for i <= j {
-		if nums[i] == val {
-			nums[i], nums[j] = nums[j], nums[i]
-			j--
-			i++
-		} else {
-			i++
+	slow := 0
+	for i := 0; i < len(nums); i++ {
+		// 如果 快指针的值不等于 val
+		// 就将数据放到慢指针所指的位置
+		if nums[i] != val {
+			nums[slow] = nums[i]
+			slow++
 		}
 	}
-	fmt.Println(nums)
-	return j
+	// nums = nums[:slow]
+	return slow
 }
