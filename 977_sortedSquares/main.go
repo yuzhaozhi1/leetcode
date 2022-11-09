65daee98299399cc156e6efff430e91b8cc778d7
yuzhaozhi1 <75839232+yuzhaozhi1@users.noreply.github.com>
2022-11-09 23:26:00 +0800

    feat: add

---
+++ 977_sortedSquares/main.go
@@ -0,0 +1,21 @@
+package main
+
+func sortedSquares(nums []int) []int {
+
+	tmp := make([]int, len(nums))
+	i := 0
+	j := len(nums) - 1
+	k := len(nums) - 1
+	for i <= j {
+		if nums[i]*nums[i] > nums[j]*nums[j] {
+			tmp[k] = nums[i] * nums[i]
+			i++
+		} else {
+			tmp[k] = nums[j] * nums[j]
+			j--
+		}
+		k--
+
+	}
+	return tmp
+}
